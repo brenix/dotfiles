d5286aac6f0ee89c9b4c8313d33a1ed9036fc7d5
Paul Nicholson <brenix@gmail.com>
2019-08-24 23:00:08 -0700

    Remove completion

--- zsh/.zsh.d/completions.zsh
+++ zsh/.zsh.d/completions.zsh
@@ -1,7 +1,3 @@
 compdef _systemctl s=systemctl
 compdef _kubectl k=kubectl
 compdef _terraform t=terraform
-if [[ $(which infractl &>/dev/null) ]]; then
-  source <(infractl completion zsh)
-  compdef _infractl infractl i=infractl
-fi
