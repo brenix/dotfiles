1a940a462fdb252f007009b23a9bdcf0b46607ee
Paul Nicholson <brenix@gmail.com>
2019-08-31 22:16:12 -0700

    Fix z function

--- zsh/.zsh.d/functions/z.zsh
+++ zsh/.zsh.d/functions/z.zsh
@@ -1,22 +1,9 @@
 # z
-unalias z
+unalias z 2> /dev/null
 z() {
-  if [[ ! ${#${(z)BUFFER}} -eq 0 && -o auto_cd ]]; then
-    zle .accept-line
-  fi
-  if [[ -z "$*" ]]; then
-    cd "$(_z -l 2>&1 | fzf +s --tac | sed 's/^[0-9,.]* *//')"
-  else
-    _last_z_args="$@"
-    _z "$@"
-  fi
-}
-
-zz() {
-  if [[ ! ${#${(z)BUFFER}} -eq 0 && -o auto_cd ]]; then
-    zle .accept-line
-  fi
-  cd "$(_z -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf -q "$_last_z_args")"
+  [ $# -gt 0 ] && _z "$*" && return
+  zle .accept-line
+  cd "$(_z -l 2>&1 | fzf --nth 2.. +s --tac --query "${*##-* }" | sed 's/^[0-9,.]* *//')"
 }
 
 zle -N z
