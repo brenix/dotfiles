e8f2f22456f9f8b69b4fdabed0720da88c2ceebd
Paul Nicholson <brenix@gmail.com>
2019-09-22 12:51:26 -0700

    Use dircolors file again

--- zsh/.zsh.d/dircolors.zsh
+++ zsh/.zsh.d/dircolors.zsh
@@ -1,6 +1,9 @@
-if $(command -v vivid >/dev/null); then
-  if [ -f ${HOME}/.config/vivid/themes/faded.yml ]; then
-    export LS_COLORS=$(vivid generate faded)
-  fi
-fi
+# if $(command -v vivid >/dev/null); then
+#   if [ -f ${HOME}/.config/vivid/themes/faded.yml ]; then
+#     export LS_COLORS=$(vivid generate faded)
+#   fi
+# fi
 
+if [ -f ${HOME}/.dircolors ]; then
+  eval $(dircolors -b $HOME/.dircolors)
+fi
