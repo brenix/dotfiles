83a1a8c2beeab0d5814f1c1db7f907d60513b83d
Paul Nicholson <brenix@gmail.com>
2019-12-15 10:02:32 -0800

    Misc fixes

--- openbox/.config/openbox/autostart.sh
+++ openbox/.config/openbox/autostart.sh
@@ -4,8 +4,12 @@
 # to run user-specific things.
 polybar main -r &
 
-$HOME/.fehbg
+#$HOME/.fehbg
 #xsetroot -solid "#3B4252"
+hashwall -f '#3b4252' -b '#2e3440' -s 12
 
 # restore wal settings
 #wal -R
+
+# fun with hacker sounds
+buckle -p ~/.bucklespring &
