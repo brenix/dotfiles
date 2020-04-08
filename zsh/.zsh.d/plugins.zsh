d46453fcc839f5db67526d4c59381af34722ad9a
Paul Nicholson <brenix@gmail.com>
2020-04-07 21:39:44 -0700

    Add git-extras plugin

--- zsh/.zsh.d/plugins.zsh
+++ zsh/.zsh.d/plugins.zsh
@@ -28,6 +28,10 @@ zinit snippet OMZ::plugins/git/git.plugin.zsh
 zinit ice wait lucid
 zinit light "paulirish/git-open"
 
+# git-extras plugin
+zinit ice as"program" pick"$ZPFX/bin/git-*" src"etc/git-extras-completion.zsh" make"PREFIX=$ZPFX"
+zinit light tj/git-extras
+
 # golang
 zinit ice wait lucid
 zinit snippet OMZ::plugins/golang/golang.plugin.zsh
