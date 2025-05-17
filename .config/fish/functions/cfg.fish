1d50be61297fdf7af5871f03692745f39c29ca3f
Paul Nicholson <brenix@gmail.com>
2025-05-17 09:09:48 -0700

    rename function

--- .config/fish/functions/cfg.fish
+++ .config/fish/functions/cfg.fish
@@ -1,9 +1,9 @@
 # https://www.atlassian.com/git/tutorials/dotfiles
-function d
+function cfg
     command git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
 end
 
-# Prevent default completions for the `dot` command. It will
+# Prevent default completions for the `cfg` command. It will
 # recurse into directories and basically hang forever.
-complete -e -c d
-complete -c d -f
+complete -e -c cfg
+complete -c cfg -f
