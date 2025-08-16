821bd43cdfbbcdc176ff693a36b905764f9f74e8
Paul Nicholson <brenix@gmail.com>
2025-08-15 19:01:21 -0700

    fzf colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -81,6 +81,8 @@ status is-interactive; and begin
     alias zad 'ls -d */ | xargs -I {} zoxide add {}'
 
     fzf --fish | source
+    set -gx FZF_DEFAULT_OPTS "--ansi --color=bg:-1,bg+:#333333,spinner:6,hl:7,fg:7,header:6,info:7,pointer:1,marker:0,prompt:2,hl+:2"
+
     zoxide init fish | source
     starship init fish | source
 
