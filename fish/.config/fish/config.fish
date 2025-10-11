59f722dc90a160166a66cbd434fe5730a06d8fbf
Paul Nicholson <brenix@gmail.com>
2025-10-11 09:06:48 -0700

    generated theming

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -84,7 +84,7 @@ status is-interactive; and begin
     # -- CLI tools
     if command -q fzf
         fzf --fish | source
-        set -gx FZF_DEFAULT_OPTS "--ansi --color=bg:-1,bg+:-1,spinner:6,hl:7,fg:7,header:6,info:7,pointer:1,marker:0,prompt:2,hl+:2"
+        set -gx FZF_DEFAULT_OPTS "--ansi --color=bg:0,bg+:-1,spinner:6,hl:7,fg:7,header:6,info:7,pointer:1,marker:0,prompt:2,hl+:2"
     end
 
     if command -q zoxide
