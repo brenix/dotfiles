985f44c23c3463af6e0ec49b7c9347d089fbe764
Paul Nicholson <brenix@gmail.com>
2025-12-26 08:48:09 -0800

    fixes

--- fish/.config/fish/functions/bind_wlpaste.fish
+++
@@ -1,4 +0,0 @@
-function bind_wlpaste
-    commandline -t -- (wl-paste -p -n)
-    commandline -f repaint
-end
--- fish/.config/fish/functions/remove.fish
+++ fish/.config/fish/functions/remove.fish
@@ -1,8 +1,9 @@
-function remove
+function remove -w pacman -w xbps-remove
+    set -l packages $argv
     if type -q pacman
-        sudo pacman -Rnsc
+        sudo pacman -Rnsc $packages
     else if type -q xbps-remove
-        sudo xbps-remove -R
+        sudo xbps-remove -R $packages
     else
         return 1
     end
