f2b042bf20f4b18aafe5b6ec8ff9be61c3d651d0
Paul Nicholson <brenix@gmail.com>
2026-05-25 14:09:11 -0700

    update colors

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -63,7 +63,7 @@ status is-interactive; and begin
     alias ip 'ip -color=auto'
     alias kubectl kubecolor
     # alias ls 'ls --color=auto --group-directories-first'
-    alias ls 'eza --group-directories-first --git --color=never'
+    alias ls 'eza --group-directories-first --git --color=auto'
     alias la 'ls -Al'
     alias l 'ls -l'
     alias ll 'ls -l'
